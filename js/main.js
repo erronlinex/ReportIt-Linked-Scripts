20a230e93cde3dd493a9e0e3c74c5a435ae5c3c0
erronlinex <shamir.khan@islington.gov.uk>
2016-01-11 10:45:10 +0000

    get url param incident value as lowcase and alert

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
+
 $(document).ready(main);
 
 function main(){
+  var incident = getURLParameter('incident').lowercase();
 // Link to scripts
-alert('test');
+alert(incident);
 }
